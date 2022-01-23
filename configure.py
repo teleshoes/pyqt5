fff292dbc805b96c52b68264106e870f90127686
Elliot Wolk <elliot.wolk@gmail.com>
2022-01-23 14:27:24 -0500

    configure.py: remove unsupported -B flag from `sip` (ancient)

--- configure.py
+++ configure.py
@@ -2454,8 +2454,8 @@ def generate_sip_module_code(target_config, verbose, parts, tracing, mname, fata
         argv.append('-f')
 
     # Make sure any unknown Qt version gets treated as the latest Qt v5.
-    argv.append('-B')
-    argv.append('Qt_6_0_0')
+    #####argv.append('-B')
+    #####argv.append('Qt_6_0_0')
 
     if target_config.prot_is_public:
         argv.append('-P');
